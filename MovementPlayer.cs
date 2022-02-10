800a1b286e81cfaf066d8505e08de119db8068f4
Panic48 <strahov.r@mail.ru>
2022-02-10 11:22:50 +0300

    Try8

--- MovementPlayer.cs
+++ MovementPlayer.cs
@@ -12,7 +12,11 @@ public class MovementPlayer : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
 
-    private float _valueForRunAnimation;
+    private float _axisY = 0;
+    private float _axisZ = 0;
+    private float _runAnimation = 1;
+    private float _stopAnimation = 0;
+    private float _valueForAnimator;
     private bool _touchGround;
     private const string Jump = nameof(Jump);
     private const string Ground = nameof(Ground);
@@ -33,32 +37,31 @@ public class MovementPlayer : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && _touchGround)
         {
-            Debug.Log(123);
             _animator.SetTrigger(Jump);
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _powerJump);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _valueForRunAnimation = 1;
+            _valueForAnimator = _runAnimation;
 
-            _animator.SetFloat(Speed, _valueForRunAnimation);
+            _animator.SetFloat(Speed, _valueForAnimator);
             _spriteRenderer.flipX = false;
-            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            transform.Translate(_speed * Time.deltaTime, _axisY, _axisZ);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            _valueForRunAnimation = 1;
+            _valueForAnimator = _runAnimation;
 
-            _animator.SetFloat(Speed, _valueForRunAnimation);
+            _animator.SetFloat(Speed, _valueForAnimator);
             _spriteRenderer.flipX = true;
-            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            transform.Translate(_speed * Time.deltaTime * -1, _axisY, _axisZ);
         }
     }
 
     private void IdleAnimation()
     {
-        _valueForRunAnimation = 0;
-        _animator.SetFloat(Speed, _valueForRunAnimation);
+        _valueForAnimator = _stopAnimation;
+        _animator.SetFloat(Speed, _valueForAnimator);
     }
 
     private void OnCollisionStay2D(Collision2D collision)
